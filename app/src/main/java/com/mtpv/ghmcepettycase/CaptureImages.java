fd867b1406ca54902724c239969313fdb72ebbce
Srinivas <srinivaskasani.mtpv@gmail.com>
2017-12-19 15:07:48 +0530

    latest code from aManohar 19/12/2017

--- app/src/main/java/com/mtpv/ghmcepettycase/CaptureImages.java
+++ app/src/main/java/com/mtpv/ghmcepettycase/CaptureImages.java
@@ -65,7 +65,7 @@ public class CaptureImages extends Activity implements LocationListener {
    	// The minimum distance to change Updates in meters
    	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
    	// The minimum time between updates in milliseconds
-   	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+   	private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute
        LocationManager m_locationlistner;
    	android.location.Location location;
 
